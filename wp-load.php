8868b8212cd3173df1b16a394fba16686457efc4
Andre Capelache <andre@webfatorial.com>
2014-05-22 19:20:08 -0300

    WordPress 3.9.1 pt-br

--- wp-load.php
+++ wp-load.php
@@ -25,38 +25,43 @@ error_reporting( E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_ERROR | E_W
 
 if ( file_exists( ABSPATH . 'wp-config.php') ) {
 
-	/** The config file resides in ABSPATH */
-	require_once( ABSPATH . 'wp-config.php' );
+    /** The config file resides in ABSPATH */
+    require_once( ABSPATH . 'wp-config.php' );
 
 } elseif ( file_exists( dirname(ABSPATH) . '/wp-config.php' ) && ! file_exists( dirname(ABSPATH) . '/wp-settings.php' ) ) {
 
-	/** The config file resides one level above ABSPATH but is not part of another install */
-	require_once( dirname(ABSPATH) . '/wp-config.php' );
+    /** The config file resides one level above ABSPATH but is not part of another install */
+    require_once( dirname(ABSPATH) . '/wp-config.php' );
+
+} elseif ( file_exists( dirname(ABSPATH) . '/../wp-config.php' ) && ! file_exists( dirname(ABSPATH) . '/wp-settings.php' ) ) {
+
+    /** The config file resides one level above ABSPATH but is not part of another install */
+    require_once( dirname(ABSPATH) . '/../wp-config.php' );
 
 } else {
 
-	// A config file doesn't exist
+    // A config file doesn't exist
 
-	define( 'WPINC', 'wp-includes' );
-	define( 'WP_CONTENT_DIR', ABSPATH . 'wp-content' );
-	require_once( ABSPATH . WPINC . '/load.php' );
-	require_once( ABSPATH . WPINC . '/version.php' );
+    define( 'WPINC', 'wp-includes' );
+    define( 'WP_CONTENT_DIR', ABSPATH . 'wp-content' );
+    require_once( ABSPATH . WPINC . '/load.php' );
+    require_once( ABSPATH . WPINC . '/version.php' );
 
-	wp_check_php_mysql_versions();
-	wp_load_translations_early();
+    wp_check_php_mysql_versions();
+    wp_load_translations_early();
 
-	// Standardize $_SERVER variables across setups.
-	wp_fix_server_vars();
+    // Standardize $_SERVER variables across setups.
+    wp_fix_server_vars();
 
-	require_once( ABSPATH . WPINC . '/functions.php' );
+    require_once( ABSPATH . WPINC . '/functions.php' );
 
-	$path = wp_guess_url() . '/wp-admin/setup-config.php';
+    $path = wp_guess_url() . '/wp-admin/setup-config.php';
 
-	// Die with an error message
-	$die  = __( "There doesn't seem to be a <code>wp-config.php</code> file. I need this before we can get started." ) . '</p>';
-	$die .= '<p>' . __( "Need more help? <a href='http://codex.wordpress.org/Editing_wp-config.php'>We got it</a>." ) . '</p>';
-	$die .= '<p>' . __( "You can create a <code>wp-config.php</code> file through a web interface, but this doesn't work for all server setups. The safest way is to manually create the file." ) . '</p>';
-	$die .= '<p><a href="' . $path . '" class="button button-large">' . __( "Create a Configuration File" ) . '</a>';
+    // Die with an error message
+    $die  = __( "There doesn't seem to be a <code>wp-config.php</code> file. I need this before we can get started." ) . '</p>';
+    $die .= '<p>' . __( "Need more help? <a href='http://codex.wordpress.org/Editing_wp-config.php'>We got it</a>." ) . '</p>';
+    $die .= '<p>' . __( "You can create a <code>wp-config.php</code> file through a web interface, but this doesn't work for all server setups. The safest way is to manually create the file." ) . '</p>';
+    $die .= '<p><a href="' . $path . '" class="button button-large">' . __( "Create a Configuration File" ) . '</a>';
 
-	wp_die( $die, __( 'WordPress &rsaquo; Error' ) );
+    wp_die( $die, __( 'WordPress &rsaquo; Error' ) );
 }
